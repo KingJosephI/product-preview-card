c1f8bb681c6a8294aa6cec57f106eec86779655f
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-09-28 12:47:03 +0100

    feat: add responsive mobile view

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Card.css';
-import productDesktop from '../../images/image-product-desktop.jpg';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Card = () => {
   return (
     <div className="card">
-      <div className="card__container">
-        <img src={productDesktop} alt="Product" />
-      </div>
+      <div className="card__image"></div>
       <div className="card__description">
         <span>PERFUME</span>
         <h1>Gabrielle Essence Eau De Parfum</h1>
@@ -15,11 +13,14 @@ const Card = () => {
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
-        <div>
+        <div className="card__prices">
           <strong>$149.99</strong>
           <span>$169.99</span>
         </div>
-        <button>Add to Cart</button>
+        <button>
+          <AiOutlineShoppingCart className="card__cart-icon" />
+          Add to Cart
+        </button>
       </div>
     </div>
   );
